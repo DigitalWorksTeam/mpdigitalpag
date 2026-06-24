13477b541e91aae372cc3e5c2a9851ebc5d126a1
Erick Rivera <yomellamoerick.771@gmail.com>
2026-06-24 19:12:29 -0400

    seccion de partners completada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mpdigital.cl',
+  integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()]
   }
